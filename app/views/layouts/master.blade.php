c6fa4f571ac3bf7d00ffbe95265dc42f8f875bb9
Patrick Reilly <preilly@php.net>
2014-05-07 16:07:15 -0700

    add navbar

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -9,10 +9,39 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- CSS are placed here -->
         {{ HTML::style('css/bootstrap.css') }}
-
+        <style>
+        @section('styles')
+            body {
+                padding-top: 60px;
+            }
+        @show
+        </style>
     </head>
 
     <body>
+        <!-- Navbar -->
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+
+                    <a class="brand" href="#">Laravel</a>
+
+                    <!-- Everything you want hidden at 940px or less, place within here -->
+                    <div class="nav-collapse collapse">
+                        <ul class="nav">
+                            <li><a href="{{{ URL::to('') }}}">Home</a></li>
+                        </ul> 
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <!-- Container -->
         <div class="container">
 
