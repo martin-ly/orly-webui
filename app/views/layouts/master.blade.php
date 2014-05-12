051305ae05175d1b9eb7515d82e895120522a696
Michael Park <mcypark@gmail.com>
2014-05-12 01:38:38 -0400

    add console to the nav bar.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -21,12 +21,12 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Dashboard</a>
+            <a class="navbar-brand" href="/">Dashboard</a>
           </div>
           <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
+              <li class="active"><a href="/">Home</a></li>
+              <li><a href="/console">Console</a></li>
               <li><a href="#">Link</a></li>
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
