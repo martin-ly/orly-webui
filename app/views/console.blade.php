98c825e536c68f69b3162ac6bb3c0448d7fe6586
Michael Park <mcypark@gmail.com>
2014-05-12 01:35:38 -0400

    improve ui of the web console.

--- app/views/console.blade.php
+++ app/views/console.blade.php
@@ -6,11 +6,29 @@
 @stop
 
 @section('content')
-<div>
-  {{ Form::textarea('query', 'Hello World!'); }}
+<div class='form-group'>
+  {{ Form::label('OrlyScript:'); }}
+  {{ Form::textarea('query', 'new session;', array('disabled',
+                                                   'class' => 'form-control',
+                                                   'id' => 'query')); }}
 </div>
-<div>
-  {{ Form::submit('Run'); }}
+<div class='form-group'>
+  {{ Form::label('Result:'); }}
+  {{ Form::textarea('reuslt', '', array('disabled',
+                                        'class' => 'form-control',
+                                        'id' => 'result')); }}
+</div>
+<div class='btn-group'>
+  {{ Form::button('Connect', array('class' => 'btn btn-default',
+                                   'id' => 'connect')); }}
+  {{ Form::button('Disconnect', array('disabled',
+                                      'class' => 'btn btn-default',
+                                      'id' => 'disconnect')); }}
+</div>
+<div class='btn-group'>
+  {{ Form::button('Run', array('disabled',
+                               'class' => 'btn btn-default',
+                               'id' => 'run')); }}
 </div>
 @stop
 
--- public/js/console.js
+++ public/js/console.js
@@ -1,14 +1,40 @@
 define(['jquery'], function($) {
-  if (!'WebSocket' in window) {
-    alert('WebSockets not supported by your browser.');
-    return;
-  }  // if
-  var websocket = new WebSocket('ws://echo.websocket.org/');
-  websocket.onmessage = function(result) {
-    alert('Result: ' + result.data);
-  };
-  $("input[value='Run']").bind('click', function() {
-    query = $("textarea[name='query']").val();
+  function toggle_ui(is_connected) {
+    $('#connect').prop('disabled', is_connected);
+    $('#query').prop('disabled', !is_connected);
+    $('#disconnect').prop('disabled', !is_connected);
+    $('#run').prop('disabled', !is_connected);
+  }
+  var websocket = null;
+  $('#connect').click(function() {
+    if (!'WebSocket' in window) {
+      alert('WebSockets not supported by your browser.');
+      return;
+    }  // if
+    websocket = new WebSocket('ws://echo.websocket.org');
+    // TODO: Talk to the orlyi websocket interface.
+    // websocket = new WebSocket('ws://127.0.0.1:8082/');
+    websocket.onopen = function(result) {
+      toggle_ui(true /* is_connected */);
+    };
+    websocket.onclose = function(result) {
+      toggle_ui(false /* is_connected */);
+    };
+    websocket.onerror = function(result) {
+      alert("Failed to connect.");
+    };
+    websocket.onmessage = function(result) {
+      // TODO: Use $.parseJSON to parse the data.
+      //       orlyi doesn't seem to be returning
+      //       well-formatted JSON at this point.
+      $('#result').val(result.data);
+    };
+  });
+  $('#disconnect').click(function() {
+    websocket.close();
+  });
+  $('#run').click(function() {
+    query = $("#query").val();
     websocket.send(query);
   });
 });
