9e485db88057dfe5815e6bf93f0457a108469f99
Michael Park <mcypark@gmail.com>
2014-05-15 16:26:34 -0400

    updated ui, used hello_world and database as examples for now.

--- app/views/console.blade.php
+++ app/views/console.blade.php
@@ -6,29 +6,38 @@
 @stop
 
 @section('content')
-<div class='form-group'>
-  {{ Form::label('OrlyScript:'); }}
-  {{ Form::textarea('query', 'new session;', array('disabled',
-                                                   'class' => 'form-control',
-                                                   'id' => 'query')); }}
+<div class='page-header'>
+  <h1>Sample Datasets <small>Click to load and play</small></h1>
+  <div class='btn-group'>
+    {{ Form::button('Connect', array('class' => 'btn btn-default',
+                                     'id' => 'connect')); }}
+    {{ Form::button('Disconnect', array('class' => 'btn btn-default',
+                                        'id' => 'disconnect')); }}
+  </div>
+  <div class='btn-group' data-toggle='buttons'>
+    <label id='hello_world' class='btn btn-default dataset'>
+      <input type='radio'> Hello World
+    </label>
+    <label id='database' class='btn btn-default dataset'>
+      <input type='radio'> Database
+    </label>
+  </div>
 </div>
-<div class='form-group'>
-  {{ Form::label('Result:'); }}
-  {{ Form::textarea('reuslt', '', array('disabled',
+{{ Form::textarea('orlyscript', '', array('disabled',
+                                          'class' => 'form-control',
+                                          'id' => 'orlyscript')); }}
+<select id='function' class='form-control input-medium'></select>
+{{ Form::text('args', '', array('class' => 'form-control',
+                                'id' => 'args')); }}
+{{ Form::button('Run', array('class' => 'btn btn-default',
+                             'id' => 'run')); }}
+<div class='panel panel-default'>
+  <div class='panel-heading'>
+    <h3 class='panel-title'><b>Log</b></h3>
+  </div>
+  {{ Form::textarea('log', '', array('disabled',
                                         'class' => 'form-control',
-                                        'id' => 'result')); }}
-</div>
-<div class='btn-group'>
-  {{ Form::button('Connect', array('class' => 'btn btn-default',
-                                   'id' => 'connect')); }}
-  {{ Form::button('Disconnect', array('disabled',
-                                      'class' => 'btn btn-default',
-                                      'id' => 'disconnect')); }}
-</div>
-<div class='btn-group'>
-  {{ Form::button('Run', array('disabled',
-                               'class' => 'btn btn-default',
-                               'id' => 'run')); }}
+                                        'id' => 'log')); }}
 </div>
 @stop
 
--- public/js/console.js
+++ public/js/console.js
@@ -1,39 +1,132 @@
-define(['jquery'], function($) {
+// Static package information.
+//   Next step would be to send the source to the server and get it to compile
+//   the package, etc and provide these information.
+//   The list of functions for example are available in `<package_name>.orly.sig`.
+packages = {
+  'hello_world' :
+    {'source' : '/* Hello World! */\n' +
+                '\n' +
+                'hello_world = (\"Hello, world! My name is \" + name + \".\") where {\n' +
+                '  name = given::(str);\n' +
+                '};\n',
+     'package_num' : 0,
+     'functions' : ['hello_world']},
+  'database' :
+    {'source' : '/* Database */\n' +
+                '\n' +
+                'package #1;\n' +
+                '\n' +
+                '/* Reads an integer from a location */\n' +
+                "read_val = (*<['values', n]>::(int)) where {\n" +
+                '    n = given::(int);\n' +
+                '};\n' +
+                '\n' +
+                '/* Writes an integer to a location */\n' +
+                'write_val = ((true) effecting {\n' +
+                "  new <['values', n]> <- x;\n" +
+                '} ) where {\n' +
+                '  n = given::(int);\n' +
+                '  x = given::(int);\n' +
+                '};\n' +
+                '\n' +
+                '/* conditional append function */\n' +
+                'conditional_append = (((true) effecting {\n' +
+                '  *<[n]>::([int]) += [x];\n' +
+                '} if *<[n]>::([int]?) is known else (true) effecting {\n' +
+                '  new <[n]> <- [x];\n' +
+                '} ))\n' +
+                'where {\n' +
+                '  n = given::(int);\n' +
+                '  x = given::(int);\n' +
+                '};',
+     'package_num' : 1,
+     'functions' : ['read_val', 'write_val', 'conditional_append']}
+};
+
+define([ 'jquery', 'bootstrap' ], function($) {
+  var websocket = null;
+  // Append text to the log box.
+  function log(text) {
+    $('#log').val(function(idx, val) {
+      return val + text + '\n';
+    });
+  }
+  // Send a message via web sockets with a onmessage handler.
+  // Note. Used to simulate synchronous send calls.
+  function send(msg, onmessage) {
+    websocket.onmessage = function(resp) {
+      log('received: ' + resp.data);
+      if (onmessage) {
+        onmessage(resp);
+      }  // if
+    };
+    websocket.send(msg);
+    log('sent: ' + msg);
+  }
+  // Set the functions options.
+  function set_functions(options) {
+    $('#function').empty();
+    $.each(options, function(key, val) {
+      $('#function').append($('<option></option>')
+                    .attr('value', val).text(val));
+    });
+  }
+  // Toggle the UI based on the state of the console.
   function toggle_ui(is_connected) {
     $('#connect').prop('disabled', is_connected);
-    $('#query').prop('disabled', !is_connected);
     $('#disconnect').prop('disabled', !is_connected);
-    $('#run').prop('disabled', !is_connected);
+    $('.dataset').attr('disabled', !is_connected);
+    if (!is_connected) {
+      $('.dataset').removeClass('active');
+      $('#run').prop('disabled', true);
+    }  // if
   }
-  var websocket = null;
+  // Connect button.
   $('#connect').click(function() {
     if (!'WebSocket' in window) {
       alert('WebSockets not supported by your browser.');
       return;
     }  // if
     websocket = new WebSocket('ws://127.0.0.1:8082/');
-    websocket.onopen = function(result) {
-      toggle_ui(true /* is_connected */);
+    websocket.onopen = function(resp) {
+      toggle_ui(true);
     };
-    websocket.onclose = function(result) {
-      toggle_ui(false /* is_connected */);
+    websocket.onclose = function(resp) {
+      toggle_ui(false);
     };
-    websocket.onerror = function(result) {
+    websocket.onerror = function(resp) {
       alert("Failed to connect.");
     };
-    websocket.onmessage = function(result) {
-      // TODO: Use $.parseJSON to parse the data.
-      //       orlyi doesn't seem to be returning
-      //       well-formatted JSON at this point.
-      $('#result').val(result.data);
-    };
   });
+  // Disconnect button.
   $('#disconnect').click(function() {
-    websocket.close();
+    send('exit;', function(resp) { websocket.close(); });
   });
-  $('#run').click(function() {
-    query = $("#query").val();
-    websocket.send(query);
+  // The packages that we have available.
+  $.each(packages, function(name, info) {
+    button = $('#' + name);
+    button.click(function() {
+      if (button.hasClass('active')) {
+        return;
+      }  // if
+      $('#orlyscript').val(info.source);
+      send('new session;', function(resp) {
+        send('new fast private pov;', function(resp) {
+          var data = $.parseJSON(resp.data);
+          var id = data.result;
+          send('install ' + name + '.' + info.package_num + ';', function(resp) {
+            set_functions(info.functions);
+            $('#run').unbind('click').click(function() {
+              send('try {' + id + '} ' + name + ' ' +
+                   $('#function').val() + ' ' + $('#args').val() + ';');
+            });
+            $('#run').prop('disabled', false);
+          });
+        });
+      });
+    });
   });
+  $('.btn').button();  // Activate buttons.
+  toggle_ui(false);  // Start in a disconnected state.
 });
 
