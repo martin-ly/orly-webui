e21ce22ea50122d71bfdd56c7bddfbf7fa3b3d6f
Cody Maloney <cmaloney@theoreticalchaos.com>
2014-06-12 16:16:20 -0700

    Remove unused bits of navigation menus, make home more accurate

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -6,6 +6,7 @@
 @stop
 
 @section('content')
-<h1>Hello World!</h1>
-<p>This page is created using a master template.</p>
+<h1>Welcome to the Orly Demo!</h1>
+<p>TODO: Add witty promotional info about what all Orly can do for you here!</p>
+<p>Go to the <a href="/console">Console</a> to get started</p>
 @stop
